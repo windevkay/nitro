e03271dcbd834bd1928c30e47968a44b28b8b4dd
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-11-12 17:30:24 -0700

    adjusted tests, pg connection and app state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use nitro::configuration::get_configuration;
 use nitro::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to get app configuration");
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connection_pool)?.await
 }
