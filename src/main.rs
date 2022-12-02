38fdb06da3c3ca31a7ab66ae0c289874fe09b56e
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-12-01 22:08:29 -0700

    setting up logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use nitro::configuration::get_configuration;
 use nitro::startup::run;
 use sqlx::PgPool;
@@ -5,6 +6,9 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // surface all logs from info level and above
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("Failed to get app configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
